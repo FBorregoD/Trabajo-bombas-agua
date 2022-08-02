a098c7f69c4b1d8d7dba6f635709a7d1369b59a0
FBorregoD <92597749+FBorregoD@users.noreply.github.com>
2022-08-02 22:36:19 +0200

    Latest change

--- utils/funciones.py
+++ utils/funciones.py
@@ -89,7 +89,11 @@ def simplifica_categoricas(df,obj,por_mayor_valor_min=20,per_umbral=5):
     return res
 
 def agrupa_categoricas(df,obj,por_mayor_valor_min=4,per_umbral=5):
-    # devuelve una columna donde se han simplificado aquellos valores de esa columna, eliminado aquellos con existencia por debajo de 4% y quedándose sólo con los cinco primeros.
+    """
+    Devuelve una columna donde se han simplificado aquellos valores de esa columna, 
+    eliminado aquellos con existencia por debajo del % en 'per_umbral=5' 
+    y quedándose sólo con los 'por_mayor_valor_min=4' primeros.
+    """
     diccio=simplifica_categoricas(df,obj,por_mayor_valor_min,per_umbral)
     salida=df[obj].map(dict(zip(diccio["valor"],diccio[obj+"_s"])))
     return(salida)
@@ -178,6 +182,7 @@ def grafica_tanzania(df,ruta,grupo,size=12):
 
     BBox = [df.longitude.min(),   df.longitude.max(),      
          df.latitude.min(), df.latitude.max()]
+    print(BBox)
 
     fig, ax = plt.subplots(figsize = (size,size))
     groups = df.groupby(grupo)
@@ -188,3 +193,350 @@ def grafica_tanzania(df,ruta,grupo,size=12):
     ax.set_xlim(BBox[0],BBox[1])
     ax.set_ylim(BBox[2],BBox[3])
     ax.imshow(img_map, zorder=0, extent = BBox, aspect= 'equal')
+
+def lugar_mas_cercano(lugar,tipo="euclidea",verbose=False):
+    """
+    Función creada para buscar la distancia más cercana de una matriz de dos columnas. Es extremadamente lenta.
+    """
+    print("método nuevo, distancia máxmimo 500 km")
+    if tipo=="euclidea":
+        modo_tipo=1
+    elif tipo=="gps":
+        modo_tipo=2
+    else:
+        modo_tipo=3
+    cont=0
+    if type(lugar)==pd.DataFrame:
+        region=lugar.values[:,2]
+        lugar=lugar.values[:,0:2]
+    
+
+
+    lugares=len(lugar)
+    dist_cerca=np.zeros(lugares)
+    rango=range(lugares)
+    min_inf=np.Inf
+    r=6370
+
+    for i in rango:
+        cont = cont+1
+        if (verbose):
+            print(f"{cont} de {lugares}, van {round(100*cont/lugares,3)}%")
+        minimo=min_inf
+        for j in rango:
+            if i==j or region[i]!=region[j]:
+                continue
+            if modo_tipo== 1:  
+                dist=np.linalg.norm(lugar[i] - lugar[j])
+            elif modo_tipo== 2: 
+                lat1,lon1=lugar[i]
+                lat2,lon2=lugar[j]
+                dlon = lon2 - lon1
+                dlat = lat2 - lat1
+                a = np.power(np.sin(dlat / 2),2) + np.cos(lat1) * np.cos(lat2) * np.power(np.sin(dlon / 2)**2,2)
+                c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+                dist= r*c
+            else:print("la variable tipo debe ser o euclidea o gps")
+            
+            if dist < minimo:
+                minimo=dist
+        if minimo >500:
+            minimo=500
+        dist_cerca[i]=minimo
+
+    dist_cerca=dist_cerca.tolist()
+    return(dist_cerca)
+
+def lugar_mas_cercano_xtest(lugar_conocido,lugar_desconocido,tipo="euclidea",verbose=False):
+    """
+    Función creada para buscar la distancia más cercana de una matriz de dos columnas.
+    Busca las distancias de los puntos X a los puntos de lugar conocido sin tener en cuenta los otros puntos desconocidos.
+    Es extremadamente lenta.
+    Esta recibe dos dataframes: la de lugares ya conocidos y la de lugares desconocidos.
+    """
+    if tipo=="euclidea":
+        modo_tipo=1
+    elif tipo=="gps":
+        modo_tipo=2
+    else:
+        modo_tipo=3
+    
+    cont=0
+
+    region_con=lugar_conocido.values[:,2]
+    lugar_con=lugar_conocido.values[:,0:2]
+    region_des=lugar_desconocido.values[:,2]
+    lugar_des=lugar_desconocido.values[:,0:2]
+
+    lugares_con=len(lugar_con)
+    lugares_des=len(lugar_des)
+
+    dist_cerca=np.zeros(lugares_des)
+    rango_con=range(lugares_con)
+    rango_des=range(lugares_des)
+    min_inf=np.Inf
+    r=6370
+
+    for i in rango_des:
+        cont = cont+1
+        if (verbose):
+            print(f"{cont} de {lugares_des}, van {round(100*cont/lugares_des,3)}%")
+        minimo=min_inf
+        for j in rango_con:
+            if region_des[i]!=region_con[j]:
+                continue
+            if modo_tipo== 1:  
+                dist=np.linalg.norm(lugares_des[i] - lugar_con[j])
+            elif modo_tipo== 2: 
+                lat1,lon1=lugar_des[i]
+                lat2,lon2=lugar_con[j]
+                dlon = lon2 - lon1
+                dlat = lat2 - lat1
+                a = np.power(np.sin(dlat / 2),2) + np.cos(lat1) * np.cos(lat2) * np.power(np.sin(dlon / 2)**2,2)
+                c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+                dist= r*c
+            else:print("la variable tipo debe ser o euclidea o gps")
+            
+            if dist < minimo:
+                minimo=dist
+        if minimo >500:
+            minimo=500
+        dist_cerca[i]=minimo
+
+    dist_cerca=dist_cerca.tolist()
+    return(dist_cerca)
+
+
+def aplica_dummies(df,lista_categoricas):
+    print("aplicamos dummies") 
+    if lista_categoricas!=[]:
+        dummies=pd.DataFrame(index=df.index)
+        for columna in lista_categoricas:
+            print(f"columna: {columna}")
+            dummi=pd.get_dummies(df[columna],prefix=columna)
+            dummies=dummies.join(dummi)
+        df_dm=df.copy()
+        df_dm.drop(columns=lista_categoricas,inplace=True)
+        df_dm=df_dm.join(dummies)
+    else:
+        df_dm=df.copy()
+    return df_dm
+
+
+import pandas as pd
+import datetime as dt
+import joblib
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+def transforma_X(X_ruta,y_train_ruta,x_train_ruta,carpeta_datos):
+        
+    ruta=carpeta_datos
+    # Primer dataset de entrenamiento
+
+    # y_train es una lista con dos columnas, id y estado de la bomba
+    y_train_filename=y_train_ruta
+    df_ytrain=pd.read_csv(ruta+y_train_filename)
+
+    # x_train es una lista con los datos de cada bomba, sin el estado.
+    x_train_filename=x_train_ruta
+    df_xtrain=pd.read_csv(ruta+x_train_filename)
+
+    # segundo dataset que se usará en el proceso de machine learning
+    x_test_filename=X_ruta
+    df_xtest=pd.read_csv(ruta+x_test_filename)
+    df_xtest.set_index('id')
+    df_xtest.sort_index(inplace=True)
+
+    # Fusionamos nuestras dos primeras bases de datos
+    df_train=df_xtrain.merge(df_ytrain,how='inner',on='id',sort=True)
+    df_train.set_index('id')
+    df_train.sort_index(inplace=True)    
+
+    # Comprobamos valores duplicados y llenamos NAN
+    df_train.drop_duplicates(inplace=True)
+    df_xtest.drop_duplicates(inplace=True)
+
+    df_train=fill_object_nan(df_train)
+    df_xtest=fill_object_nan(df_xtest)
+
+    # Introducimos columnas de información para el date recorded.
+    def add_date_columns(df):
+        df["date_recorded"]=pd.to_datetime(df['date_recorded'],format='%Y-%m-%d',errors='coerce')
+        df["date_recorded_year"]=df["date_recorded"].dt.year
+        df["date_recorded_month"]=df["date_recorded"].dt.month
+
+        # Establecemos 1999 como año de cosntrucción de aquellos bombas que no se conocen.
+        df.construction_year=np.where(df.construction_year>0,df.construction_year,1999)
+        df.population=np.where(df.population>0,df.population,\
+            round(np.median(df.population.loc[df.population>0].mean()),0))
+        return df
+
+    df_train=add_date_columns(df_train)
+    df_xtest=add_date_columns(df_xtest)
+
+    
+    for col in tipos_col(df_train)["obj"]:
+        # simplificamos las variables categóricas, limitándolas a los diez valores por variable con presencia de > 2%.
+        df_train[col] = agrupa_categoricas(df_train,col,10,0)
+        
+        # conservamos mismos tipos escogidos en xtest
+        if col in df_xtest.columns:
+            valores_categoricas=df_train[col].unique()
+            df_xtest[col] = df_xtest[col].apply(lambda x: x if x in valores_categoricas else 'unknown')
+
+        
+    # Simplificamos el target para obtener 2 en primicia
+    df_train['status_group_simpl']=df_train['status_group']
+    df_train['status_group_simpl']=np.where(df_train['status_group_simpl']=='functional','functional','non functional')
+
+    #Substutimos valores extra por "desconocidos"
+    df_train.replace("other","unknown",inplace=True)
+    df_xtest.replace("other","unknown",inplace=True)
+
+    " Existen muchos valores con longitud y latitud cero. sin embargo, tienen una región asignada. Por ello, vamos a asignarles valores al azar entre los puntos de cada región"
+
+    df_train_local=df_train.loc[df_train["longitude"]!=0]
+
+    regiones_maximas=df_train_local[["gps_height","longitude","latitude","region_code"]].groupby(by=["region_code"]).max().add_suffix("_max")
+    regiones_minimas=df_train_local[["gps_height","longitude","latitude","region_code"]].groupby(by=["region_code"]).min().add_suffix("_min")
+
+    regiones=regiones_maximas.join(regiones_minimas)
+    
+    #inputamos valores de colocación basados en región en una nueva variable (df_train_one)
+    df_train_zero=df_train.loc[df_train["longitude"]==0]
+    df_xtest_zero=df_xtest.loc[df_xtest["longitude"]==0]
+
+    var_imputables=["gps_height","longitude","latitude"]
+
+    for region in regiones.index:
+        for variable in var_imputables:
+
+            min=regiones.loc[[region]][variable+"_min"].values[0]
+            max=regiones.loc[[region]][variable+"_max"].values[0]
+
+            tam_train=df_train_zero[variable].loc[df_train_zero["region_code"]==region].shape
+            tam_xtest=df_xtest_zero[variable].loc[df_xtest_zero["region_code"]==region].shape
+
+            valores_train=np.random.random_sample(size=tam_train)*(max-min)+np.ones(tam_train)*min
+            valores_xtest=np.random.random_sample(size=tam_xtest)*(max-min)+np.ones(tam_xtest)*min
+
+            df_train_zero[variable].loc[df_train_zero["region_code"]==region]=valores_train
+            df_xtest_zero[variable].loc[df_xtest_zero["region_code"]==region]=valores_xtest
+
+    df_train_one=df_train.copy()
+    df_xtest_one=df_xtest.copy()
+
+    df_train_one.loc[df_train_zero.index, var_imputables]=df_train_zero[var_imputables]
+    df_xtest_one.loc[df_xtest_zero.index, var_imputables]=df_xtest_zero[var_imputables]
+    
+    
+    # Añadimos quién es el pozo más cercano. 
+    # Para esto nos basaremos UNICAMENTE en los pozos de la misma región del set de entrenamiento. 
+    # Esto se dará incluso si estamos en el set de test.
+    # Es un proceso largo, puede durar más de una hora. Por eso, miraré si he calculado ya el punto.
+
+    # Miramos si ya he trabajado estos datos antes. Si no, los creamos. Esta operación puede llevar hasta una hora.
+    archivo_gps_train='df_train_one_gps.pkl'
+    path= Path(ruta+archivo_gps_train)
+    if path.is_file():
+        print("archivo de datos train gps encontrado")
+        print(f"leyendo de {ruta+archivo_gps_train}")
+        df_train_one_gps=joblib.load(ruta+archivo_gps_train).copy()
+        df_train_one=df_train_one.join(df_train_one_gps['dist_mas_cercano'],how='inner')
+    else:
+        df_train_one=lugar_mas_cercano(df_train_one[["longitude","latitude","region_code"]],"gps",True)
+        joblib.dump(df_train_one, ruta+archivo_gps_train)
+
+    # Repetimos el paso con los datos del dataset objetivo.
+    archivo_gps_xtest='df_xtest_prueba.pkl'
+    path= Path(ruta+archivo_gps_xtest)
+    if path.is_file():
+        print("archivo de datos xtest gps encontrado")
+        print(f"leyendo de {ruta+archivo_gps_xtest}")
+        df_xtest_one_gps=joblib.load(ruta+archivo_gps_xtest)
+        df_xtest_one=df_xtest_one.join(df_xtest_one_gps['dist_mas_cercano'],how='inner')
+    else:
+        df_xtest_one["dist_mas_cercano"]=lugar_mas_cercano_xtest(df_train_one[["longitude","latitude","region_code"]],\
+            df_xtest_one[["longitude","latitude","region_code"]],"gps",True)
+        joblib.dump(df_xtest_one, ruta+archivo_gps_xtest)
+
+
+    #Guardamos variables, guardamos indices como indice.
+
+    df_train_one.set_index('id',inplace=True)
+    df_xtest_one.set_index('id',inplace=True)
+
+    # Categorías a trabajar
+    lista_numericas=['amount_tsh', 'construction_year', 'longitude', 'latitude', 'gps_height','population','dist_mas_cercano']
+    listas_targets=['status_group','status_group_simpl']
+
+    lista_categoricas=["quantity_group","extraction_type_class","waterpoint_type"]
+    listas=[lista_numericas,lista_categoricas,listas_targets]
+    listas=sum(listas,[])
+    listas_test=sum([lista_numericas,lista_categoricas],[])
+
+    df_train_two=df_train_one[listas]
+    df_xtest_two=df_xtest_one[listas_test]
+
+    # Aplicamos dummies
+
+    print("aplicamos dummies a df_train")
+    df_train_two_dm=aplica_dummies(df_train_two,lista_categoricas)
+    # primero conseguido
+
+    print("aplicamos dummies a df_xtest")
+    df_xtest_three=aplica_dummies(df_xtest_two,lista_categoricas)
+    print("ambos conseguidos")
+
+    # guardamos en nueva df
+
+    df_train_two_dm['target_trio']=df_train_two_dm['status_group'].replace(['functional', 'functional needs repair','non functional'],[0, 1,2])
+    df_train_two_dm['target_duo']=df_train_two_dm['status_group_simpl'].replace(['functional', 'functional needs repair','non functional'],[0, 1,1])
+    df_train_three=df_train_two_dm.drop(columns=['status_group','status_group_simpl'])
+
+    # si hay columnas extra de dummmies en df_train_two que no hay en df_xtest, las añadimos ahora, con valor cero en todas ellas.
+
+    col_comunes=set(df_train_three.columns).symmetric_difference(set(df_xtest_three.columns)).symmetric_difference(set(['target_duo','target_trio']))
+    for col in col_comunes:
+        df_xtest_three[col] = 0
+
+    col_sin_target=list(set(df_train_three.columns).intersection(set(df_xtest_three.columns)))
+    col_sin_target.sort()
+    col_con_target=col_sin_target[:]
+    col_con_target.extend(['target_duo','target_trio'])
+
+    df_train_three=df_train_three[col_con_target]
+    df_xtest_three=df_xtest_three[col_sin_target]
+
+    return df_xtest_three
+
+def predice_X(df_xtest_three,modelo_ruta,df_scores_ruta,ruta_results,nombre_salida):
+    pipelines=joblib.load(ruta_results+modelo_ruta)
+    scores_df=joblib.load(ruta_results+df_scores_ruta)
+
+    X_real_test=df_xtest_three.loc[:,~df_xtest_three.columns.isin(['target_duo', 'target_trio'])]
+    
+    # Predecimos empleando el modelo encontrado con mejor posición.
+    indice_mejor=scores_df[['Accuracy']].idxmax()
+    salida_prediccion=pipelines[indice_mejor.values[0]].predict(X_real_test)
+
+
+    df_xtest_three["respuesta"]=salida_prediccion
+    df_xtest_three["status_group"]=df_xtest_three["respuesta"].replace([0, 1,2],['functional', 'functional needs repair','non functional'])
+ 
+    file=ruta_results+nombre_salida
+    df_xtest_three['status_group'].to_csv(file)
+
+    return df_xtest_three['status_group']
